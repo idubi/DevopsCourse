2dceb7322283ea2d0ecc54d37ca9bde3d55dc0c8
idubim <idubi.m@gmail.com>
2023-06-07 03:57:20 +0300

    try fixbranch

--- lesson03 2/assignment/server/blueprints/file_routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-# Create a Blueprint instance
-file = Blueprint('file', __name__)
-
-
-@file.route('/')
-def index():
-    return 'Hello from the files blueprint!'
--- lesson03 2/assignment/server/test/test_flask_endpoints.py
+++
@@ -1,8 +0,0 @@
-from lesson03.assignment.exercise import *
-
-print_ex_boundary("ex_09_a")
-ex_09_a()
-print_ex_boundary("ex_09_b")
-ex_09_b()
-print_ex_boundary("ex_09_c")
-ex_09_c()
